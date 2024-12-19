b61ae9e60f5cf4502e09c50dc8a765cb87a9bd69
onyo9 <onyo9ssafy@gmail.com>
2024-12-19 14:42:05 +0900

    code challenge

--- App.js
+++ App.js
@@ -7,15 +7,20 @@ import {
   TextInput,
   ScrollView,
   Alert,
+  Button,
 } from 'react-native';
 import { theme } from './color';
 import { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import Fontisto from '@expo/vector-icons/Fontisto';
+import Checkbox from 'expo-checkbox';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const STORAGE_KEY = "@toDos"
+const STORAGE_PAGE = "@working"
 
 // 1. 완료 상태 구분 (completed)
+
 // 2. 수정이 가능하게 해줘 (modify)
 // 3. 나갔다 들어왔을 떄, Work 화면 or Travel 화면 중 최근에 들어왔던 화면으로 가게 해줘줘
 
@@ -23,14 +28,28 @@ const STORAGE_KEY = "@toDos"
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState();
+  const [editText, setEditText] = useState();
   const [toDos, setToDos] = useState({});
+  const [editing, setEditing] = useState("");
   useEffect(() => {
     loadToDos();
   }, [])
 
-  const travel = () => setWorking(false);
-  const work = () => setWorking(true);
+  const travel = async () => {
+    setWorking(false);
+    await AsyncStorage.setItem(STORAGE_PAGE, JSON.stringify(false))
+  }
+
+  const work = async () => {
+    setWorking(true);
+    await AsyncStorage.setItem(STORAGE_PAGE, JSON.stringify(true))
+  }
+
   const onChangeText = (payload) => setText(payload);
+  const onChangeEditText = (payload) => {
+    console.log(payload)
+    setEditText(payload);
+  }
   const saveToDos = async (toSave) => {
     await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave))
   }
@@ -38,7 +57,9 @@ export default function App() {
   const loadToDos = async () => {
     try {
       const s = await AsyncStorage.getItem(STORAGE_KEY);
+      const p = await AsyncStorage.getItem(STORAGE_PAGE)
       setToDos(JSON.parse(s))
+      setWorking(JSON.parse(p))
     } catch (error) {
       console.error("There's an error!")
     }
@@ -48,14 +69,33 @@ export default function App() {
     if (text == "") {
       return
     }
-    // save to do
     const newToDos = {
       ...toDos,
-      [Date.now()]: { text, working }
+      [Date.now()]: { text, working, completed: false }
     };
     setToDos(newToDos);
     await saveToDos(newToDos);
     setText("");
+  }
+
+  const toggleToDo = async (key) => {
+    const newToDos = { ...toDos };
+    newToDos[key].completed = !newToDos[key].completed; 
+    setToDos(newToDos); 
+    await saveToDos(newToDos);
+  };
+
+  const setEditToDo = (key) => {
+    setEditing(key);
+    setEditText(toDos[key].text)
+  }
+
+  const completeEditToDo = async (key) => {
+    const newToDos = { ...toDos };
+    newToDos[key].text = editText;
+    setToDos(newToDos);
+    setEditing("");
+    await saveToDos(newToDos)
   }
 
   const deleteToDo = async (key) => {
@@ -98,12 +138,36 @@ export default function App() {
       </View>
       <ScrollView>
         {Object.keys(toDos).map(key =>
-          toDos[key].working == working ? (<View style={styles.toDo} key={key}>
-            <Text style={styles.toDoText}>{toDos[key].text}</Text>
-            <TouchableOpacity onPress={() => deleteToDo(key)}>
-              <Fontisto name="trash" size={18} color={theme.toDoBg} />
-            </TouchableOpacity>
-          </View>) : null
+          toDos[key].working == working ? (
+            <View style={styles.toDo} key={key}>{
+              editing == key ? (
+                <TextInput flex={10}
+                  returnKeyType='done'
+                  value={editText}
+                  onSubmitEditing={() => completeEditToDo(key)}
+                  onChangeText={onChangeEditText}
+                  style={styles.editInput}
+                />
+              ) : (
+                <Text flex={10} style={[styles.toDoText, toDos[key].completed && { textDecorationLine: 'line-through' }]} >{toDos[key].text}</Text>
+              )
+            }
+              <TouchableOpacity onPress={() => { setEditToDo(key) }}>
+                <AntDesign name="edit" size={24} color="white" />
+              </TouchableOpacity>
+              <View flex={1} style={styles.checkbox}>
+                <Checkbox
+                  onValueChange={() => toggleToDo(key)}
+                  value={toDos[key].completed}
+                />
+              </View>
+              <View flex={1}>
+                <TouchableOpacity onPress={() => deleteToDo(key)}>
+                  <Fontisto name="trash" size={18} color="white" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          ) : null
         )}
       </ScrollView>
     </View>
@@ -133,19 +197,29 @@ const styles = StyleSheet.create({
     marginVertical: 20,
     fontSize: 18,
   },
+  editInput: {
+    backgroundColor: "white",
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+    borderRadius: 5,
+    fontSize: 16,
+  },
   toDo: {
     flexDirection: "row",
     backgroundColor: theme.grey,
     marginBottom: 10,
-    paddingHorizontal: 40,
+    paddingHorizontal: 30,
     paddingVertical: 20,
     borderRadius: 15,
     alignItems: 'center',
-    justifyContent: "space-between",
   },
   toDoText: {
     color: "white",
     fontSize: 16,
-    fontWeight: "500"
+    fontWeight: "500",
   },
+  checkbox: {
+    marginHorizontal: 20,
+    marginVertical: 6,
+  }
 });
